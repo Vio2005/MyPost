771bc85655bd735f0cfa64086697e89013afd186
Vio2005 <pyaepyaeviolet2005@gmail.com>
2025-05-11 18:22:23 +0630

    posting

--- mypost/mypost/urls.py
+++ mypost/mypost/urls.py
@@ -17,14 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from posts.views import postdata
-from posts.views import postdetail
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/',postdata,name='posts'),
-    path('detail/<str:pk>/',postdetail,name='detail')
+   
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mypost/posts/views.py
+++ mypost/posts/views.py
@@ -6,8 +6,3 @@ def postdata(request):
     post_data=Post.objects.all()
     context={'post_data':post_data}
     return render(request,'post.html',context)
-def postdetail(request,pk):
-    data=Post.objects.filter(id= pk)
-    getdata=Post.objects.get(id=pk) #single query
-    context={'data':data, 'getdata':getdata}
-    return render(request,'postdetails.html',context)
